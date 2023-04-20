56b8898cd2d88849c1ad3bad256ace3a8a08e65b
leftzhf <acemagic123@outlook.com>
2023-04-21 00:28:49 +0800

    fix: 修改图标样式

--- src/views/visitor/chat/index.vue
+++ src/views/visitor/chat/index.vue
@@ -3,35 +3,34 @@
     <el-container>
       <el-header>
         <div>
-<!--          &lt;!&ndash; 数据为空 &ndash;&gt;-->
-<!--          <div v-if="messageList || messageList.length === 0" class="loading">-->
-<!--            <div style="margin-top: 300px; font-size: 16px;">-->
-<!--              <span>未查找到聊天记录</span>-->
-<!--            </div>-->
-<!--          </div>-->
+          <!--          &lt;!&ndash; 数据为空 &ndash;&gt;-->
+          <!--          <div v-if="messageList || messageList.length === 0" class="loading">-->
+          <!--            <div style="margin-top: 300px; font-size: 16px;">-->
+          <!--              <span>未查找到聊天记录</span>-->
+          <!--            </div>-->
+          <!--          </div>-->
 
-<!--          &lt;!&ndash; 刚进页面第一次加载 &ndash;&gt;-->
-<!--          <div v-if="messageList && messageList.length === 0" class="loading">-->
-<!--            <div style="margin-top: 300px; font-size: 16px;">-->
-<!--              <div>加载中...</div>-->
-<!--            </div>-->
-<!--          </div>-->
-
+          <!--          &lt;!&ndash; 刚进页面第一次加载 &ndash;&gt;-->
+          <!--          <div v-if="messageList && messageList.length === 0" class="loading">-->
+          <!--            <div style="margin-top: 300px; font-size: 16px;">-->
+          <!--              <div>加载中...</div>-->
+          <!--            </div>-->
+          <!--          </div>-->
           <!-- 导航栏 -->
           <div v-if="messageList && messageList.length > 0" class="title">
-            <span >您正在与客服{{contact.nickname}}对话</span>
+            <span>客服{{ contact.nickname }}为您服务</span>
           </div>
           <div v-else class="title">
-            <span>等待转接</span>
+            <span>您好，请稍等，客服正在赶来的路上~</span>
           </div>
-          <div class="tooltip-base-box">
-            <el-tooltip content="评分" placement="bottom" effect="light">
-              <i class="fa fa-star-half-full"  @click="showRateDialog()"/>
+          <div class="opr-wrapper">
+            <el-tooltip  content="评分" placement="bottom" effect="light">
+              <i class="fa fa-star-half-full" @click="showRateDialog()"/>
             </el-tooltip>
-            <el-tooltip content="留言" placement="bottom" effect="light">
+            <el-tooltip  content="留言" placement="bottom" effect="light">
               <i class="fa fa-envelope-o" @click="showLeaveDialog()"></i>
             </el-tooltip>
-            <el-tooltip content="结束会话" placement="bottom" effect="light">
+            <el-tooltip  content="结束会话" placement="bottom" effect="light">
               <i class="fa fa-close" @click="closeChat()"></i>
             </el-tooltip>
           </div>
@@ -59,13 +58,13 @@
                 >
                   <!-- 时间 -->
                   <div class="time">
-                    <span > {{ parseTime(message.createdAt, "{y}-{m}-{d} {h}:{i}:{s}")}}</span>
-                    <span v-if="message.status ===1 && isOneself(message) " >未读</span>
-                    <span v-if="message.status ===2 && isOneself(message)" >已读</span>
+                    <span> {{ parseTime(message.createdAt, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
+                    <span v-if="message.status ===1 && isOneself(message) ">未读</span>
+                    <span v-if="message.status ===2 && isOneself(message)">已读</span>
                   </div>
                   <!-- 系统提示 -->
                   <div v-if="message.type === '10000'" class="time system">
-                    <span v-html="message.content" />
+                    <span v-html="message.content"/>
                   </div>
                   <div v-else :class="'main' + (isOneself(message) ? ' self' : '')">
                     <!-- 头像 -->
@@ -76,7 +75,7 @@
                     >
 
                     <!-- 文本 -->
-                    <div v-if="message.type == 1" v-emotion="message.content" class="text" />
+                    <div v-if="message.type == 1" v-emotion="message.content" class="text"/>
 
                     <!-- 图片 -->
                     <div v-else-if="message.type == 2" class="text">
@@ -96,24 +95,24 @@
 
         </div>
         <div class="input">
-<!--          <el-row>-->
-<!--            <el-col :span="20">-->
-              <el-input
-                v-model="inputText"
-                type="textarea"
-                placeholder="协助TA"
-                :rows="5"
-                @keyup.enter.native="sendMessage"
-              >
-<!--                <template #suffix>-->
-<!--                  <el-button type="primary" @click="sendMessage">发送</el-button>-->
-<!--                </template>-->
-              </el-input>
-<!--            </el-col>-->
-<!--            <el-col :span="4">-->
-<!--              <el-button type="primary" @click="sendMessage">发送</el-button>-->
-<!--            </el-col>-->
-<!--          </el-row>-->
+          <!--          <el-row>-->
+          <!--            <el-col :span="20">-->
+          <el-input
+            v-model="inputText"
+            type="textarea"
+            placeholder="协助TA"
+            :rows="5"
+            @keyup.enter.native="sendMessage"
+          >
+            <!--                <template #suffix>-->
+            <!--                  <el-button type="primary" @click="sendMessage">发送</el-button>-->
+            <!--                </template>-->
+          </el-input>
+          <!--            </el-col>-->
+          <!--            <el-col :span="4">-->
+          <!--              <el-button type="primary" @click="sendMessage">发送</el-button>-->
+          <!--            </el-col>-->
+          <!--          </el-row>-->
         </div>
       </el-main>
     </el-container>
@@ -123,6 +122,10 @@
     <!-- 离线留言dialog -->
     <el-dialog :visible.sync="leaveDialogVisible" :close-on-press-escape="false">
       <im-leave ref="im_leave"></im-leave>
+    </el-dialog>
+    <!-- 满意度dialog -->
+    <el-dialog :visible.sync="rateDialogVisible" :close-on-press-escape="false">
+      <im-rate ref="im_rate"></im-rate>
     </el-dialog>
   </div>
 </template>
@@ -139,6 +142,7 @@ import { parseTime } from '@/utils/date'
 import imRate from './imRate.vue'
 import imLeave from './imLeave.vue'
 import imTransfer from './imTransfer.vue'
+
 export default {
   components: {
     imRate: imRate,
@@ -468,274 +472,299 @@ export default {
 </script>
 
 <style scoped>
-  /* 消息列表 */
-  .chat-container {
-    background-color: #efefef;
-    z-index: 100;
-    overflow: hidden;
-    min-width: 300px;
-    margin: 0 auto;
-    padding: 0;
-    position: relative;
-  }
+/*.opr-wrapper .tooltip-right {*/
+/*  left: 100%;*/
+/*  transform: translateX(100px);*/
+/*}*/
+.opr-wrapper {
+  position: absolute;
+  right: 4%;
+  top: 3%;
+  transform: translateY(-60%);
+  font-size: 16px;
+  cursor: pointer;
+  color: #ffffff;
+  /*right: 20px;*/
+  /*font-size: 16px;*/
+  /*cursor: pointer;*/
+}
+.fa {
+  margin-left: 10px;
+}
+.tooltip-right{
+  left: 100%;
+  transform: translateX(100px);
+}
 
-  .title {
-    background: #1060b4;
-    text-align: center;
-    color: #fff;
-    width: 100%;
-    height: 50px;
-    line-height: 50px;
-    font-size: 14px;
-  }
+/* 消息列表 */
+.chat-container {
+  background-color: #efefef;
+  z-index: 100;
+  overflow: hidden;
+  min-width: 300px;
+  margin: 0 auto;
+  padding: 0;
+  position: relative;
+}
 
-  .loading {
-    text-align: center;
-    color: #b0b0b0;
-    line-height: 100px;
-  }
+.title {
+  position: relative;
+  background: #1060b4;
+  text-align: center;
+  color: #fff;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  font-size: 14px;
+}
 
-  .message {
-    padding: 10px 15px;
-    background-color: #f5f5f5;
-  }
+.loading {
+  text-align: center;
+  color: #b0b0b0;
+  line-height: 100px;
+}
 
-  .message li {
-    margin-bottom: 15px;
+.message {
+  padding: 10px 15px;
+  background-color: #f5f5f5;
+}
+
+.message li {
+  margin-bottom: 15px;
+  left: 0;
+  position: relative;
+  display: block;
+}
+
+.message .time {
+  margin: 10px 0;
+  text-align: center;
+}
+
+.message .text {
+  display: inline-block;
+  position: relative;
+  max-width: calc(100% - 75px);
+  min-height: 35px;
+  line-height: 2.1;
+  font-size: 15px;
+  padding: 6px 10px;
+  text-align: left;
+  word-break: break-all;
+  background-color: #fff;
+  color: #000;
+  border-radius: 4px;
+  box-shadow: 0px 1px 7px -5px #000;
+}
+
+.message .avatar {
+  float: left;
+  margin: 0 10px 0 0;
+  border-radius: 3px;
+  background: #fff;
+  width: 45px;
+  height: 45px;
+}
+
+.message .time > span {
+  display: inline-block;
+  padding: 0 5px;
+  font-size: 12px;
+  color: #fff;
+  border-radius: 2px;
+  background-color: #dadada;
+}
+
+.message .system > span {
+  padding: 4px 9px;
+  text-align: left;
+}
+
+.message .text:before {
+  content: " ";
+  position: absolute;
+  top: 9px;
+  right: 100%;
+  border: 6px solid transparent;
+  border-right-color: #fff;
+}
+
+.message .main {
+  text-align: left;
+}
+
+.message .self {
+  text-align: right;
+}
+
+.message .self .avatar {
+  float: right;
+  margin: 0 0 0 10px;
+}
+
+.message .self .text {
+  background-color: #9eea6a;
+}
+
+.message .self .text:before {
+  right: inherit;
+  left: 100%;
+  border-right-color: transparent;
+  border-left-color: #9eea6a;
+}
+
+.message .image {
+  max-width: 200px;
+}
+
+img.static-emotion-gif,
+img.static-emotion {
+  vertical-align: middle !important;
+}
+
+.an-move-left {
+  left: 0;
+  animation: moveLeft 0.7s ease;
+  -webkit-animation: moveLeft 0.7s ease;
+}
+
+.an-move-right {
+  left: 0;
+  animation: moveRight 0.7s ease;
+  -webkit-animation: moveRight 0.7s ease;
+}
+
+@keyframes moveRight {
+  0% {
+    left: -20px;
+    opacity: 0;
+  }
+  100% {
     left: 0;
-    position: relative;
-    display: block;
+    opacity: 1;
   }
+}
 
-  .message .time {
-    margin: 10px 0;
-    text-align: center;
+@-webkit-keyframes moveRight {
+  0% {
+    left: -20px;
+    opacity: 0;
   }
-
-  .message .text {
-    display: inline-block;
-    position: relative;
-    max-width: calc(100% - 75px);
-    min-height: 35px;
-    line-height: 2.1;
-    font-size: 15px;
-    padding: 6px 10px;
-    text-align: left;
-    word-break: break-all;
-    background-color: #fff;
-    color: #000;
-    border-radius: 4px;
-    box-shadow: 0px 1px 7px -5px #000;
-  }
-
-  .message .avatar {
-    float: left;
-    margin: 0 10px 0 0;
-    border-radius: 3px;
-    background: #fff;
-    width: 45px;
-    height: 45px;
-  }
-
-  .message .time > span {
-    display: inline-block;
-    padding: 0 5px;
-    font-size: 12px;
-    color: #fff;
-    border-radius: 2px;
-    background-color: #dadada;
-  }
-
-  .message .system > span {
-    padding: 4px 9px;
-    text-align: left;
-  }
-
-  .message .text:before {
-    content: " ";
-    position: absolute;
-    top: 9px;
-    right: 100%;
-    border: 6px solid transparent;
-    border-right-color: #fff;
-  }
-
-  .message .main {
-    text-align: left;
-  }
-
-  .message .self {
-    text-align: right;
-  }
-
-  .message .self .avatar {
-    float: right;
-    margin: 0 0 0 10px;
-  }
-
-  .message .self .text {
-    background-color: #9eea6a;
-  }
-
-  .message .self .text:before {
-    right: inherit;
-    left: 100%;
-    border-right-color: transparent;
-    border-left-color: #9eea6a;
-  }
-
-  .message .image {
-    max-width: 200px;
-  }
-
-  img.static-emotion-gif,
-  img.static-emotion {
-    vertical-align: middle !important;
-  }
-
-  .an-move-left {
+  100% {
     left: 0;
-    animation: moveLeft 0.7s ease;
-    -webkit-animation: moveLeft 0.7s ease;
+    opacity: 1;
   }
+}
 
-  .an-move-right {
+@keyframes moveLeft {
+  0% {
+    left: 20px;
+    opacity: 0;
+  }
+  100% {
     left: 0;
-    animation: moveRight 0.7s ease;
-    -webkit-animation: moveRight 0.7s ease;
+    opacity: 1;
   }
+}
 
-  @keyframes moveRight {
-    0% {
-      left: -20px;
-      opacity: 0;
-    }
-    100% {
-      left: 0;
-      opacity: 1;
-    }
+@-webkit-keyframes moveLeft {
+  0% {
+    left: 20px;
+    opacity: 0;
   }
-
-  @-webkit-keyframes moveRight {
-    0% {
-      left: -20px;
-      opacity: 0;
-    }
-    100% {
-      left: 0;
-      opacity: 1;
-    }
-  }
-
-  @keyframes moveLeft {
-    0% {
-      left: 20px;
-      opacity: 0;
-    }
-    100% {
-      left: 0;
-      opacity: 1;
-    }
-  }
-
-  @-webkit-keyframes moveLeft {
-    0% {
-      left: 20px;
-      opacity: 0;
-    }
-    100% {
-      left: 0;
-      opacity: 1;
-    }
-  }
-
-  @media (max-width: 367px) {
-    .fzDInfo {
-      width: 82%;
-    }
-  }
-
-  /* 上下拉刷新 */
-  .container-main {
-    margin: 0 auto;
-    overflow: auto;
-    overflow-x: hidden;
-    padding: 0;
-  }
-
-  .loading {
-    width: 100%;
-    height: 40px;
-    position: relative;
-    overflow: hidden;
-    text-align: center;
-    margin: 5px 0;
-    color: #999;
-    font-size: 13px;
-  }
-
-  .loader {
-    font-size: 10px;
-    margin: 8px auto;
-    text-indent: -9999em;
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    background: #999;
-    background: -moz-linear-gradient(left, #999 10%, rgba(255, 255, 255, 0) 42%);
-    background: -webkit-linear-gradient(left, #999 10%, rgba(255, 255, 255, 0) 42%);
-    background: -o-linear-gradient(left, #999 10%, rgba(255, 255, 255, 0) 42%);
-    background: -ms-linear-gradient(left, #999 10%, rgba(255, 255, 255, 0) 42%);
-    background: linear-gradient(to right, #999 10%, rgba(255, 255, 255, 0) 42%);
-    position: relative;
-    -webkit-animation: load3 1s infinite linear;
-    animation: load3 1s infinite linear;
-  }
-
-  .loader:before {
-    width: 50%;
-    height: 50%;
-    background: #999;
-    border-radius: 100% 0 0 0;
-    position: absolute;
-    top: 0;
+  100% {
     left: 0;
-    content: "";
+    opacity: 1;
   }
+}
 
-  .loader:after {
-    background: #f5f5f5;
-    width: 72%;
-    height: 75%;
-    border-radius: 68%;
-    content: "";
-    margin: auto;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
+@media (max-width: 367px) {
+  .fzDInfo {
+    width: 82%;
   }
+}
 
-  @-webkit-keyframes load3 {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
+/* 上下拉刷新 */
+.container-main {
+  margin: 0 auto;
+  overflow: auto;
+  overflow-x: hidden;
+  padding: 0;
+}
 
-  @keyframes load3 {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
+.loading {
+  width: 100%;
+  height: 40px;
+  position: relative;
+  overflow: hidden;
+  text-align: center;
+  margin: 5px 0;
+  color: #999;
+  font-size: 13px;
+}
+
+.loader {
+  font-size: 10px;
+  margin: 8px auto;
+  text-indent: -9999em;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  background: #999;
+  background: -moz-linear-gradient(left, #999 10%, rgba(255, 255, 255, 0) 42%);
+  background: -webkit-linear-gradient(left, #999 10%, rgba(255, 255, 255, 0) 42%);
+  background: -o-linear-gradient(left, #999 10%, rgba(255, 255, 255, 0) 42%);
+  background: -ms-linear-gradient(left, #999 10%, rgba(255, 255, 255, 0) 42%);
+  background: linear-gradient(to right, #999 10%, rgba(255, 255, 255, 0) 42%);
+  position: relative;
+  -webkit-animation: load3 1s infinite linear;
+  animation: load3 1s infinite linear;
+}
+
+.loader:before {
+  width: 50%;
+  height: 50%;
+  background: #999;
+  border-radius: 100% 0 0 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  content: "";
+}
+
+.loader:after {
+  background: #f5f5f5;
+  width: 72%;
+  height: 75%;
+  border-radius: 68%;
+  content: "";
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+
+@-webkit-keyframes load3 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
   }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes load3 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
 </style>
